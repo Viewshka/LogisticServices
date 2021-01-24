dc44f3cf972d2301e609aeda8c3963863a0c3c6a
Viewshka <uravip01@gmail.com>
2021-01-24 19:09:11 +0300

    Миграция: добавлены сущности Order, OrderStructure, Unit, ServiceType

---
+++ LogisticService.Core/Entities/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace LogisticService.Core.Entities
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string ShortName { get; set; }
+        public string Description { get; set; }
+    }
+}
--- LogisticService.WebUI/Migrations/20210124160626_AddEntities.Designer.cs
+++ LogisticService.WebUI/Migrations/20210124160626_AddEntities.Designer.cs
@@ -4,14 +4,16 @@ using LogisticService.Application.Common.Access;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LogisticService.WebUI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210124160626_AddEntities")]
+    partial class AddEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
